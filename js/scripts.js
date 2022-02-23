388b7aad747bde6cf8a4c7984b58912113c4cf4b
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2022-02-23 13:19:54 +0100

    canvis fets

--- js/scripts.js
+++ js/scripts.js
@@ -130,3 +130,15 @@ function canviaJap(){
   banderaIdioma.setAttribute('src', banderaTriada);
   
 }
+
+function form_validate(attr_id){
+  var result = true;
+  $('#'+attr_id).validator('validate');
+  $('#'+attr_id+' .form-group').each(function(){
+      if($(this).hasClass('has-error')){
+          result = false;
+          return false;
+      }
+  });
+  return result;
+}
